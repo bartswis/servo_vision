87561bc192a5ce9ef0a83499f1e83c8c048b5c0d
bartswis <bartswis@gmail.com>
2017-07-19 19:31:12 +0200

    sv_system: look_at_cmd added

--- sv_system/src/CS_test.cpp
+++ sv_system/src/CS_test.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "visualization_msgs/Marker.h"
 #include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/Point.h"
 #include "tf2_ros/transform_listener.h" 
 #include "tf/transform_datatypes.h"
 #include <cmath>
@@ -9,7 +10,7 @@
 
     ros::Publisher pub_marker;
     ros::Publisher pub_position;
-    ros::Publisher pub_delay;
+    ros::Publisher pub_look_at;
     std::string marker_frame;
     tf2_ros::TransformListener *tfListener;
     tf2_ros::Buffer tfBuffer;
@@ -26,12 +27,17 @@
         tf::poseMsgToTF(camera_object_pose_msg.pose, camera_object_transform);
 
         tf::Transform world_object_transform = world_camera_transform * camera_object_transform;
+        
         geometry_msgs::PoseStamped world_object_pose;
-        tf::poseTFToMsg(world_object_transform, world_object_pose.pose);
-
+        tf::poseTFToMsg(world_object_transform, world_object_pose.pose);                    
         world_object_pose.header.frame_id = marker_frame;
         world_object_pose.header.stamp = ros::Time::now();
         pub_position.publish(world_object_pose);
+        
+        // TODO object position plus movement
+        geometry_msgs::Point look_at_point;
+        look_at_point = world_object_pose.pose.position;
+        pub_look_at.publish(look_at_point);
 
         uint32_t shape = visualization_msgs::Marker::SPHERE;
         visualization_msgs::Marker marker;
@@ -58,20 +64,24 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "control_system");
     ros::NodeHandle nh("~");
 
-        nh.getParam("marker_frame", marker_frame);
-        std::string visualization_marker;
-        nh.getParam("visualization_marker", visualization_marker);
-        pub_marker = nh.advertise<visualization_msgs::Marker>(visualization_marker, 1);
+    nh.getParam("marker_frame", marker_frame);
+    std::string visualization_marker;
+    nh.getParam("visualization_marker", visualization_marker);
+    pub_marker = nh.advertise<visualization_msgs::Marker>(visualization_marker, 1);
 
-        std::string publisher;
-        nh.getParam("publisher", publisher);
-        pub_position = nh.advertise<geometry_msgs::PoseStamped>(publisher, 1);
+    std::string publisher_pos;
+    nh.getParam("publisher_pos", publisher_pos);
+    pub_position = nh.advertise<geometry_msgs::PoseStamped>(publisher_pos, 1);
+    
+    std::string publisher_cmd;
+    nh.getParam("publisher_cmd", publisher_cmd);
+    pub_look_at = nh.advertise<geometry_msgs::Point>(publisher_cmd, 1);
 
-        std::string subscriber;
-        nh.getParam("subscriber", subscriber);
-        ros::Subscriber sub = nh.subscribe(subscriber, 1, controlCallback);
+    std::string subscriber;
+    nh.getParam("subscriber", subscriber);
+    ros::Subscriber sub = nh.subscribe(subscriber, 1, controlCallback);
 
-        tfListener = new tf2_ros::TransformListener(tfBuffer);
+    tfListener = new tf2_ros::TransformListener(tfBuffer);
 
     ros::spin();
 }
